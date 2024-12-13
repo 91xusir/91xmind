656c7de3c9bc5d0587d6dc5be7ac46677f37ce5b
刘岗强 <MrLiuGangQiang@users.noreply.github.com>
2024-12-13 18:47:14 +0800

    Update authUpdate.ts

--- auth_api/src/controllers/admin/auth/authUpdate.ts
+++ auth_api/src/controllers/admin/auth/authUpdate.ts
@@ -3,14 +3,14 @@ import { Context } from "hono";
 // 获取指定的激活码
 export const authUpdate = async (c: Context) => {
   try {
-    let { id, deviceCode, tokenCode, expiryTime, isBanned } =
+    let { id, deviceCode, tokenCode, expiryTime, isBanned,extra_info } =
       await c.req.json();
     isBanned = isBanned ? isBanned : 0;
 
     await c.env.DB.prepare(
-      "UPDATE tb_auth SET deviceCode = ?1 ,tokenCode= ?2 ,expiryTime = ?3 , isBanned = ?4 WHERE id = ?5"
+      "UPDATE tb_auth SET deviceCode = ?1 ,tokenCode= ?2 ,expiryTime = ?3 , isBanned = ?4 , extra_info = ?5 WHERE id = ?6"
     )
-      .bind(deviceCode, tokenCode, expiryTime, isBanned, id)
+      .bind(deviceCode, tokenCode, expiryTime, isBanned,extra_info,id)
       .run();
 
     return c.json({

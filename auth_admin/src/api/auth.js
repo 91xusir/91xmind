00dc6cf08e83dc760bd09d699db2664c878800df
刘岗强 <MrLiuGangQiang@users.noreply.github.com>
2024-12-13 18:59:16 +0800

    修改备注

--- auth_admin/src/api/auth.js
+++ auth_admin/src/api/auth.js
@@ -6,10 +6,10 @@ export const authPageListService = ({ pageSize, currentPage }) =>
 export const authQuaryListService = ({ deviceCode, tokenCode }) =>
   request.get('/api/v1/authinfo', { params: { deviceCode, tokenCode } })
 
-export const authAddService = ({ deviceCode, tokenCode, expiryTime, isBanned }) =>
-  request.post('/api/v1/authinfo', { deviceCode, tokenCode, expiryTime, isBanned })
+export const authAddService = ({ deviceCode, tokenCode, expiryTime, isBanned, extra_info }) =>
+  request.post('/api/v1/authinfo', { deviceCode, tokenCode, expiryTime, isBanned, extra_info })
 
-export const authUpdateService = ({ id, deviceCode, tokenCode, expiryTime, isBanned }) =>
-  request.put('/api/v1/authinfo', { id, deviceCode, tokenCode, expiryTime, isBanned })
+export const authUpdateService = ({ id, deviceCode, tokenCode, expiryTime, isBanned, extra_info }) =>
+  request.put('/api/v1/authinfo', { id, deviceCode, tokenCode, expiryTime, isBanned, extra_info })
 
 export const authDeleteService = (ids) => request.delete('/api/v1/authinfo', { params: { ids } })
